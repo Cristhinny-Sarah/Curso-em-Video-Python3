b436967786dbb9a3f246e1cf5f74475c4f29bf43
Cristhinny-Sarah <cristhinnysarah@gmail.com>
2020-08-25 20:19:21 -0300

    Atualizacao do arquivo ate a aula 7

--- Mundo 1/p1.py
+++ Mundo 1/p1.py
@@ -9,7 +9,7 @@
     #ex2.: nome = 'Joao'
 # A funcao input() recebe informacoes do usuario
     #ex3.: nome = input('Qual seu nome?')
-    
+
 '''
 #DESAFIO 1 AULA 4
 nome = input('Qual seu nome? ')
@@ -47,7 +47,7 @@ print('A soma entre {} e {} e {}'.format(num1, num2, sum))
 # existem varias funcoes para teste de tipo, pesquisar mais depois
 
 '''
-#DESAFIO 2 AULA 6: faca um programa que leia algo pelo teclado e mostre
+#DESAFIO 4 AULA 6: faca um programa que leia algo pelo teclado e mostre
 # na tela o seu tipo primitivo e todas as informacoes possiveis sobre ele
 var = input('Digite algo: ')
 print('O tipo primitivo desse valor e {}'.format(type(var)))
@@ -60,4 +60,98 @@ print('Esta em minusculo? {}'.format(var.islower()))
 print('Esta capitalizada? {}'.format(var.istitle()))
 '''
 
-# 
+# ** potenciacao, // divisao inteira, % resto da divisao
+# ordem de precedencia: ()
+#                       **
+#                       *, /, //, %
+#                       +, -
+# base**potencia = pow(base,potencia)
+
+'''
+# Apenas algumas coisas que da pra fazer com o .format()
+nome = input('Qual e seu nome? ')
+print('Prazer em te conhecer {}!'.format(nome))
+print('Prazer em te conhecer {:20}!'.format(nome)) #escreve nome em 20 espacos
+print('Prazer em te conhecer {:>20}!'.format(nome)) #escreve nome em 20 espacos e alinha a direita
+print('Prazer em te conhecer {:<20}!'.format(nome)) #escreve nome em 20 espacos e alinha a esquerda
+print('Prazer em te conhecer {:^20}!'.format(nome)) #escreve nome em 20 espacos e alinha ao centro
+print('Prazer em te conhecer {:=^20}!'.format(nome)) #escreve nome em 20 espacos, alinhado ao centro e com igual em volta
+# caso eu queria que nao quebre uma linha de um print para outro basta fazer
+# print('mensagem', end = ''). Quebrar linha e \n
+
+num1 = 5
+num2 = 2
+s = num1 + num2
+d = num1/num2
+m = num1*num2
+di = num1//num2
+e = num1**num2
+print('{}, {:.3f}, {}, {}, {}'.format(s, d, m, di, e)) #a formatacao {:.3f} especifica a quantidade de casas decimais que quero que seja mostrado do numero
+'''
+
+'''
+#DESAFIO 5 AULA 7: faca um programa que leia um numero inteiro e mostre
+#na tela seu sucessor e seu antecessor
+num = int(input('Digite um numero inteiro: '))
+print('O numero {} tem {} como seu antecessor e {} como seu sucessor'.format(num, num-1, num+1))
+'''
+
+'''
+#DESAFIO 6 AULA 7: faca um algoritmo que leia um numero e mostre o seu dobro,
+#triplo e raiz quadrada
+num = float(input('Digite um numero: '))
+print('Dobro de {}: {}\nTriplo de {}: {}\nRaiz quadrada de {}: {:.3f}'.format(num, num*2, num, num*3, num, num**(1/2)))
+'''
+
+'''
+#DESAFIO 7 AULA 7: desenvolva um programa que leia as duas notas de um aluno
+#calcula e mostre a sua media
+num1 = float(input('Digite a primeira nota: '))
+num2 = float(input('Digite a segunda nota: '))
+print('A media entre {} e {} e {:.3f}'.format(num1, num2, (num1 + num2)/2))
+'''
+
+'''
+#DESAFIO 8 AULA 7: escreva um programa que leia um valor em metros e o exiba
+#convertido em centimetros e milimetros
+num = float(input('Digite um valor em metros: '))
+print('{} metros sao {} centrimetos'.format(num, num*100))
+print('{} metros sao {} milimetros'.format(num, num*1000))
+'''
+
+'''
+#DESAFIO 9 AULA 7: faca a tabuada de um numero qualquer
+#OBS.: eu trapaceei nessa questao porque fique com preguica
+num = float(input('Digite um numero: '))
+print('A tabuada do numero {} e: '.format(num))
+for i in range(10):
+    print('{} x {} = {}'.format(num, i, num*i))
+'''
+
+'''
+#DESAFIO 10 AULA 7
+reais = float(input('Quantos reais voce possui? '))
+print('Com {} reais voce consegue {} dolares, considerando que 1 dolar = 3.27 reais.'.format(reais, reais//3.27))
+'''
+
+'''
+#DESAFIO 11 AULA 7
+largura = float(input('Escreva a largura da parede: '))
+altura = float(input('Escreva a altura da parede: '))
+area = largura*altura
+print('Uma parede de {} metros quadrados precisa de {} litros de tinta para ser pintada.'.format(area, area/2))
+'''
+
+'''
+#DESAFIO 12 AULA 7
+produto = float(input('Digite o preco do produto: '))
+desconto = (produto*5)/100
+print('O preco original do produto e {} reais, com 5 por cento de desconto ele sai a {} reais.'.format(produto, produto - desconto))
+'''
+
+'''
+#DESAFIO 13 AULA 7
+salario = float(input('Qual valor do salario do funcionario? '))
+aumento = (salario*15)/100
+print('O funcionario recebia {} e agora passa a receber {} depois do aumento de 15 por cento.'.format(salario, salario + aumento))
+'''
