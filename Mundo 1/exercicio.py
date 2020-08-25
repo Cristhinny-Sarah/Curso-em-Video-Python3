4a1ba576025c26d3e396ffdfe831ab69e9dda44d
Cristhinny-Sarah <cristhinnysarah@gmail.com>
2020-08-25 15:48:20 -0300

    Primeira parte do Mundo 1 do curso de python 3

---
+++ Mundo 1/exercicio.py
@@ -0,0 +1,17 @@
+
+'''
+# Desafio 1: Faca um programa que mostra a mensagem 'Ola Mundo'
+# na tela
+print('Ola Mundo!')
+'''
+
+'''
+# Desafio 2: Faca um programa que leia o nome de uma pessoa e
+# mostre uma mensagem de boas-vindas
+nome = input('Digite seu nome: ')
+#print('E um prazer te conhecer,', nome)
+
+print('E um prazer te conhecer, {}!'.format(nome))
+# veja, essa funcao vai formatar a variavel nome para
+# caber em {}
+'''
---
+++ Mundo 1/p1.py
@@ -0,0 +1,32 @@
+#Zen do Python: import this (no terminal python)
+#Strings sao delimitadas por ' '
+    #OBS.: e possivel usar " " para delimitar strings, mas por padrao os programadores usam ' '
+#funcao print() mostra coisas na tela
+#operador de + pode ser usado para concatenar strings
+    #ex.: '7' + '4' = '74'
+#Toda variavel em python e um objeto
+# = significa recebe
+    #ex2.: nome = 'Joao'
+#funcao input() recebe informacoes do usuario
+    #ex3.: nome = input('Qual seu nome?')
+'''
+#DESAFIO 1 AULA 4
+nome = input('Qual seu nome? ')
+print('Ola ', nome, ' ! Prazer em te conhecer!')
+'''
+
+'''
+#DESAFIO 2 AULA 4
+dia = input('Dia = ')
+mes = input('Mes = ')
+ano = input('Ano = ')
+print('Voce nasceu no dia ', dia, ' de ', mes, ' de ', ano, '. Correto?')
+'''
+
+'''
+#DESAFIO 3 AULA 4
+num1 = input('Primeiro numero ')
+num2 = input('Segundo numero ')
+sum = int(num1) + int(num2)
+print(sum)
+'''
